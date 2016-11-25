0302a90721082bb46226da995cbf36eac0780cc3
Ahmed Ahres <ahres.ahmed@yahoo.fr>
2016-11-25 23:13:24 +0200

    Stuff added

--- TheResurrectionOfCoruxo/Assets/Scripts/CameraMove.cs
+++ TheResurrectionOfCoruxo/Assets/Scripts/CameraMove.cs
@@ -4,27 +4,36 @@ using System.Collections;
 public class CameraMove : MonoBehaviour {
 
 
-
+	public GameObject cube;
+	bool isDestroyed = false;
 
 
 	// Use this for initialization
 	void Start () {
 	
 	}
+
+	void OnMouseClick()
+	{
+		Destroy (cube);
+		if (cube.activeInHierarchy == false) {
+			isDestroyed = true;
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		InputControl ();
+
+		if (Input.GetKey ("d")) {
+			transform.localPosition += new Vector3 (0.05F, 0F, 0F);
+		}
+		if (Input.GetKey ("a")) {
+			transform.localPosition += new Vector3 (-0.05F, 0F, 0F);
+		}
 
 	
 	}
-
-
-	void InputControl(){
-		float vertical = Input.GetAxis ("Vertical");
-
-
-	}
+		
 
 
 
