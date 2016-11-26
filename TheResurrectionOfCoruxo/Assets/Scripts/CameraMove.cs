223df225230fde9d62d5e39a70b3ed3da4d67298
Ahmed Ahres <ahres.ahmed@yahoo.fr>
2016-11-26 08:26:54 +0200

    CameraMove

--- TheResurrectionOfCoruxo/Assets/Scripts/CameraMove.cs
+++ TheResurrectionOfCoruxo/Assets/Scripts/CameraMove.cs
@@ -6,30 +6,55 @@ public class CameraMove : MonoBehaviour {
 
 	public GameObject cube;
 	bool isDestroyed = false;
+	private Vector3 offset;
+	public float off1 = 5;
+	public float off2 = -5;
+	public float off3 = 10;
+	public float off4 = -10;
 
 
 	// Use this for initialization
 	void Start () {
-	
+		UnlockCamera (1);
+		StartCoroutine(Example());
 	}
 
-	void OnMouseClick()
-	{
-		Destroy (cube);
-		if (cube.activeInHierarchy == false) {
-			isDestroyed = true;
+	public void UnlockCamera(int offfset){
+		if (offfset == 1) {
+			if (transform.localPosition.x < off1) {
+				if (Input.GetKey ("d")) {
+					transform.localPosition += new Vector3 (0.05F, 0F, 0F);
+				}
+			}
+			if (transform.localPosition.x > -off1) {
+				if (Input.GetKey ("a")) {
+					transform.localPosition += new Vector3 (-0.05F, 0F, 0F);
+				}
+			}
 		}
+
+		if (offfset == 2) {
+			if (transform.localPosition.x < off2) {
+				if (Input.GetKey ("d")) {
+					transform.localPosition += new Vector3 (0.05F, 0F, 0F);
+				}
+			}
+			if (transform.localPosition.x > -off2) {
+				if (Input.GetKey ("a")) {
+					transform.localPosition += new Vector3 (-0.05F, 0F, 0F);
+				}
+			}
+		}
+	}
+
+	IEnumerator Example(){
+		yield return new WaitForSeconds (10);
+		UnlockCamera (2);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		if (Input.GetKey ("d")) {
-			transform.localPosition += new Vector3 (0.05F, 0F, 0F);
-		}
-		if (Input.GetKey ("a")) {
-			transform.localPosition += new Vector3 (-0.05F, 0F, 0F);
-		}
+		
 
 	
 	}
